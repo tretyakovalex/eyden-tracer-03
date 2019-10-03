201483936ae64f9da034d459a12c889096534cd4
Creator <sergej.kosov@gmail.com>
2019-10-03 23:11:30 +0200

    Add BSP Tree sceleton

---
+++ src/BoundingBox.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "ray.h"
+
+namespace {
+	inline Vec3f Min3f(const Vec3f a, const Vec3f b)
+	{
+		return Vec3f(MIN(a.val[0], b.val[0]), MIN(a.val[1], b.val[1]), MIN(a.val[2], b.val[2]));
+	}
+
+	inline Vec3f Max3f(const Vec3f a, const Vec3f b)
+	{
+		return Vec3f(MAX(a.val[0], b.val[0]), MAX(a.val[1], b.val[1]), MAX(a.val[2], b.val[2]));
+	}
+}
+
+/**
+ * @brief Bounding Box class
+ */
+class CBoundingBox
+{
+public:
+	CBoundingBox(void) = default;
+	~CBoundingBox(void)= default;
+	
+	/**
+	 * @brief Resets the bounding box
+	 * @details This function resets the member variables \b m_min and \b m_max
+	 */
+	void clear(void)
+	{
+		// --- PUT YOUR CODE HERE ---
+	}
+	
+	/**
+	 * @brief Extends the bounding box to contain point \b a
+	 * @param a A point
+	 */
+	void extend(Vec3f a)
+	{
+		// --- PUT YOUR CODE HERE ---
+	}
+	
+	/**
+	 * @brief Extends the bounding box to contain bounding box \b box
+	 * @param box The second bounding box
+	 */
+	void extend(const CBoundingBox& box)
+	{
+		// --- PUT YOUR CODE HERE ---
+	}
+	
+	/**
+	 * @brief Checks if the current bounding box overlaps with the argument bounding box \b box
+	 * @param box The secind bounding box to be checked with
+	 */
+	bool overlaps(const CBoundingBox& box)
+	{
+		// --- PUT YOUR CODE HERE ---
+		return true;
+	}
+	
+	/**
+	 * @brief Clips the ray with the bounding box
+	 * @param[in] ray The ray
+	 * @param[in,out] t0 The distance from ray origin at which the ray enters the bounding box
+	 * @param[in,out] t1 The distance from ray origin at which the ray leaves the bounding box
+	 */
+	void clip(const Ray& ray, float& t0, float& t1)
+	{
+		// --- PUT YOUR CODE HERE ---
+	}
+	
+	
+public:
+	Vec3f m_min;	///< The minimal point defying the size of the bounding box
+	Vec3f m_max;	///< The maximal point defying the size of the bounding box
+};
